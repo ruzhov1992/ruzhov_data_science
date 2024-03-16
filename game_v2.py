41d90412d67bc0bc31d7ebd316a71055c68ee8ea
Ivan <ruzhov1992@gmail.com>
2024-03-16 21:35:31 +0300

    Initial commit

---
+++ game_v2.py
@@ -0,0 +1,29 @@
+"""Игра угадай число. Computer edition"""
+
+import numpy as np
+
+def random_predict(number: int=1) ->int:
+    count =0
+
+    while True:
+        count +=1
+        predict_number = np.random.randint(1,101)
+        if number == predict_number:
+            break
+    return(count)
+print(f'Trying: {random_predict(10)}')
+
+def score_game(random_predict) -> int:
+    count_ls = []
+    np.random.seed(1)
+    random_array = np.random.randint(1, 101, size =(1000))
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f'Your algoritm try success in middle: {score}')
+    return(score)
+
+if __name__ == '__main__':
+    score_game(random_predict)
